6e6ded0e8ab06b7f379961040b6f41c52165a44e
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-05-18 09:10:58 +0530

    Modified

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,19 +2,21 @@ package com.review;
 
 public class TicTacToe {
 	
-	static void createNewBoard()
+	static char[] createNewBoard()
 	{
-		char[] board=new char[10];
+		char[] new_board=new char[10];
 		for(int pos=1;pos<10;pos++)
 		{
-			board[pos]=' ';
+			new_board[pos]=' ';
 		}
+		
+		return new_board;
 	}
 	
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Tic Tac Toe Game");
-		createNewBoard();
+		char[] board=createNewBoard();
 	}
 
 }
