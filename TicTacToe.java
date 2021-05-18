c182ed3dd65bf075905c462d92a2ab8b4180bb7a
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-05-18 08:44:11 +0530

    Added: New Board Created

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,20 @@
 package com.review;
 
 public class TicTacToe {
-
+	
+	static void createNewBoard()
+	{
+		char[] board=new char[10];
+		for(int pos=1;pos<10;pos++)
+		{
+			board[pos]=' ';
+		}
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Tic Tac Toe Game");
+		createNewBoard();
 	}
 
 }
