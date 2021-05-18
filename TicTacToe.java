85dfb4c7070a38ebfe5ee476f0a494c58122b752
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-05-18 09:40:08 +0530

    Added: Player Can Choose Its Character

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,8 @@
 package com.review;
-
+import java.util.*;
 public class TicTacToe {
+	
+	static char PLAYER,COMPUTER;
 	
 	static char[] createNewBoard()
 	{
@@ -13,10 +15,36 @@ public class TicTacToe {
 		return new_board;
 	}
 	
+	static void playerCharacter()
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Please Select A character 'X' or 'O' : ");
+		
+		PLAYER=sc.next().toUpperCase().charAt(0);
+		
+		
+		if(PLAYER=='X')
+			COMPUTER='O';
+		else if(PLAYER=='O')
+			COMPUTER='X';
+		else
+			{
+				System.out.println("Please Enter a Valid Character");
+				PLAYER=' ';
+			}
+			
+		sc.close();
+		
+
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Tic Tac Toe Game");
 		char[] board=createNewBoard();
+		playerCharacter();
+		
 	}
 
 }
